3d93073b3c1248d788eb2b8df89db98956d1e8d7
Trey Griffith <trey.griffith@gmail.com>
2013-11-25 01:49:30 -0800

    restructure some, add template updates, and the beginnings of the two-way binding

--- test/oz.js
+++ test/oz.js
@@ -20,6 +20,12 @@ describe('Rendering', function(){
     assert('Paul' == el[1].children[0].textContent);
   });
 
+  it('should use object values as array elements', function(){
+    var el = Oz.render('<div oz-each="people"><p oz-text="name"></p></div>', { people: [ {name: 'Tobi'}, {name: 'Paul'} ]});
+    assert('Tobi' == el[0].children[0].textContent);
+    assert('Paul' == el[1].children[0].textContent);
+  });
+
   it('should hide elements that have falsey values', function(){
     var el = Oz.render('<div oz-if="bool"></div>', { bool: false });
     assert(el[0].style.display === 'none');
@@ -41,13 +47,113 @@ describe('Rendering', function(){
     assert('something' == el[0].children[0].textContent);
   });
 
+  it('should render multiple attributes in one tag', function(){
+    var el = Oz.render('<div oz-attr="class:name;data-active:active"></div>', { name: 'Tobi', active: true}); 
+    assert('Tobi' == el[0].className);
+    assert('true' == el[0].getAttribute('data-active'));
+  });
+
   it('should render multiple top level elements', function(){
     var el = Oz.render('<p oz-text="name"></p><p oz-text="text"></p>', { name: 'Tobi', text: 'something'});
     assert('Tobi' == el[0].textContent);
     assert('something' == el[1].textContent);
   });
+
+  it('should call functions to get values', function(){
+    var el = Oz.render('<div oz="person"><p oz-text="name"></p></div>', { person: function () { return { name: 'Tobi' }; }});
+    assert('Tobi' == el[0].children[0].textContent);
+  });
 });
 
+describe("Updating", function() {
+  it('should keep the same dom node when updating', function(){
+    var template = Oz('<div></div>');
+
+    var el = template.render({})[0];
+    var el2 = template.update({})[0];
+
+    assert(el === el2);
+  });
+
+  it('should update text values', function(){
+
+    var template = Oz('<div><p oz-text="name"></p></div>');
+    var el = template.render({ name: 'Tobi' })[0];
+
+    assert('Tobi' == el.children[0].textContent);
+
+    template.update({ name: 'Brian' });
+
+    assert('Brian' == el.children[0].textContent);
+  });
+
+  it('should update text of a nested value', function(){
+
+    var template = Oz('<div oz="person"><p oz-text="name"></p></div>');
+    var el = template.render({ person: { name: 'Tobi' } })[0];
+
+    assert('Tobi' == el.children[0].textContent);
+
+    template.update({person: { name: 'Brian' }});
+
+    assert('Brian' == el.children[0].textContent);
+  });
+
+  it('should update text values as array elements', function(){
+    console.log("Starting text array tests");
+
+    var template = Oz('<div oz-each="names"><p oz-text="@"></p></div>');
+    var el = template.render({ names: ['Tobi', 'Paul']});
+    assert('Tobi' == el[0].children[0].textContent);
+    assert('Paul' == el[1].children[0].textContent);
+
+    template.update({names: ['Tobi', 'Brian']});
+
+    console.log(el[0]);
+    console.log(el[1]);
+
+    assert('Tobi' == el[0].children[0].textContent);
+    assert('Brian' == el[1].children[0].textContent);
+  });
+
+  it('should add new array elements', function(){
+    var template = Oz('<div oz-each="names"><p oz-text="@"></p></div>');
+    var el = template.render({ names: ['Tobi', 'Paul']});
+
+    template.update({names: ['Tobi', 'Paul', 'Brian']});
+
+    assert('Brian' == el[2].children[0].textContent);
+  });
+
+  it('should remove deleted array elements', function(){
+    var template = Oz('<div oz-each="names"><p oz-text="@"></p></div>');
+    var el = template.render({ names: ['Tobi', 'Paul', 'Brian']});
+
+    template.update({names: ['Tobi', 'Paul']});
+
+    assert(el[2] == null);
+  });
+
+  it('should show elements that have truthy values', function(){
+    var template = Oz('<div oz-if="bool"></div>');
+    var el = template.render({ bool: false });
+    assert(el[0].style.display === 'none');
+
+    template.update({bool: true});
+    assert(el[0].style.display === '');
+  });
+
+  it('should update multiple attributes in one tag', function(){
+    var template = Oz('<div oz-attr="class:name;data-active:active"></div>');
+    var el = template.render({ name: 'Tobi', active: true});
+    assert('Tobi' == el[0].className);
+    assert('true' == el[0].getAttribute('data-active'));
+
+    template.update({ name: 'Paul', active: false});
+    assert('Paul' == el[0].className);
+    assert('false' == el[0].getAttribute('data-active'));
+  });
+});
 
 /*
 describe('Oz.render(template, context)', function(){
